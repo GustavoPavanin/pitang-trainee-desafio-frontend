7f51be0c7cad1d43964a6445d8e712e92dac47a0
Gustavo Pavanin Martins <GustavoPavanin>
2022-04-12 12:07:34 -0300

    Implementando React DatePicker e Yup

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,62 +1,74 @@
-import React from "react";
-import { Formik, Field, Form } from "formik";
+import React , {useState} from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import DatePicker from "react-datepicker";
+import Schema from "../../schema";
 import "./bootstrap.min.css";
 import "./style.css";
 import "react-datepicker/dist/react-datepicker.css";
 
 const CardComponent = () => {
-
-	const startDate = new Date();
+	const currentDate = new Date();
+	const [appointmentDate, setAppointmentDate] = useState(new Date());
+	const [birthdate, setBirthdate] = useState(new Date());
 	const onSubmit = (values) =>{
 		console.log("SUBMIT", values);
-	};
-	const validate = (values) => {
-		const errors = {};
-		if(!values.name){
-			errors.name = "Name is required";
-		}
-		if(!values.email){
-			errors.email = "Email is required";
-		}
-		if(!values.birthdate){
-			errors.birthdate = "Birthdate is required";
-		}
-		return errors;
+		alert(JSON.stringify(values, null, 2));
 	};
 	return (
 		<div>
 			<Formik 
-				validateOnMount
-				validate={validate}
+				validationSchema={Schema}
 				onSubmit={onSubmit}
-				initialValues={{name: "",
+				validateOnMount
+				initialValues={{
+					name: "",
 					email: "",
 					birthdate: "",
 					appointmentDate: ""}	
 				}
 			>
-				{({ values, errors, handleChange}) => (
+				{({ values,  isValid, setFieldValue}) => (
 					<Form>
 						<div>
 							<label htmlFor="name">Name</label>
 							<Field id="name" name="name" type="text"/>
-							{ errors.name && ( <span>{errors.name}</span> ) } 
+							<ErrorMessage name="name"/>
 						</div>
 						<div>
 							<label htmlFor="email">Email Address</label>
 							<Field id="email" name="email" type="email"/>
-							{ errors.email && ( <span>{errors.email}</span> ) } 
+							<ErrorMessage name="email"/>
 						</div>
 						<div>
 							<label htmlFor="birthdate">Birthdate</label>
-							<Field id="birthdate" name="birthdate" type="date"/>
-							{ errors.birthdate && ( <span>{errors.birthdate}</span> ) } 
+							<DatePicker 
+								id="birthdate" 
+								name="birthdate" 
+								value={values.birthdate}
+								selected={birthdate}
+								onChange={(value) => {
+									setBirthdate(Date.parse(value));
+									setFieldValue("birthdate",value);
+									console.log(birthdate);
+								}}
+							/>
+							<ErrorMessage name="birthdate"/>
 						</div>
 						<div>
-							<DatePicker id="appointmentDate" selected={startDate} onChange={handleChange} value={values.appointmentDate} showTimeSelect dateFormat="Pp"/>
+							<label htmlFor="appointmentDate">Appointment Date</label>
+							<DatePicker id="appointmentDate" selected={appointmentDate} 
+								onChange={(value) => {
+									setAppointmentDate(Date.parse(value));
+									setFieldValue("appointmentDate", value);
+									console.log(appointmentDate);
+								}}
+								minDate={currentDate}
+								value={values.appointmentDate} 
+								showTimeSelect 
+								dateFormat="Pp"/>
+							<ErrorMessage name="appointmentDate"/>
 						</div>
-						<button type="submit">Submit</button>
+						<button type="submit" disabled={!isValid}>Submit</button>
 					</Form>
 				)}
 			</Formik>
