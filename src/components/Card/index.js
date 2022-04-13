25d3275bf39c3156abdeb782c7f6c01cdb79f937
Gustavo Pavanin Martins <GustavoPavanin>
2022-04-13 10:36:17 -0300

    Revert "Merge pull request #7 from GustavoPavanin/customizeFormAppointment"
    This reverts commit 310a00be0bd519504d045edd0f85909dfc6abb9a, reversing
    changes made to 16309b641381de8e1144b3fb0fff82248aa97c6d.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,9 +3,8 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import DatePicker from "react-datepicker";
 import Schema from "../../schema";
 import "react-datepicker/dist/react-datepicker.css";
-import { Card, TextField, CardContent, Typography, Button } from "@mui/material";
 
-const CardForm = () => {
+const CardComponent = () => {
 	const currentDate = new Date();
 	const [appointmentDate, setAppointmentDate] = useState(new Date());
 	const [birthdate, setBirthdate] = useState(new Date());
@@ -14,74 +13,66 @@ const CardForm = () => {
 		alert(JSON.stringify(values, null, 2));
 	};
 	return (
-		<Card>
-			<CardContent>
-				<Typography variant="h4">Schedule your vaccination date</Typography>
-				<Formik 
-					validationSchema={Schema}
-					onSubmit={onSubmit}
-					validateOnMount
-					initialValues={{
-						name: "",
-						email: "",
-						birthdate: "",
-						appointmentDate: ""}	
-					}
-				>
-					{({ values,  isValid, setFieldValue}) => (
-						<Form>
-							<div>
-								<Field id="name" name="name" type="text" 
-									as={TextField} label="Name" variant="filled" sx={{ m: 1}}
-								/>
-								<ErrorMessage name="name" />
-							</div>
-							<div>
-								<Field id="email" name="email" type="email"
-									as={TextField} label="Email Address" variant="filled" sx={{ m: 1}}
-								/>
-								<ErrorMessage name="email" />
-							</div>
-							<div>
-								<label htmlFor="birthdate">Birthdate</label>
-								<DatePicker 
-									id="birthdate" 
-									name="birthdate" 
-									maxDate={currentDate}
-									value={values.birthdate}
-									selected={birthdate}
-									onChange={(value) => {
-										setBirthdate(Date.parse(value));
-										setFieldValue("birthdate",value);
-										console.log(birthdate);
-									}}
-									className={TextField}
-								/>
-								<ErrorMessage name="birthdate"/>
-							</div>
-							<div>
-								<label htmlFor="appointmentDate">Appointment Date</label>
-								<DatePicker id="appointmentDate" selected={appointmentDate} 
-									onChange={(value) => {
-										setAppointmentDate(Date.parse(value));
-										setFieldValue("appointmentDate", value);
-										console.log(appointmentDate);
-									}}
-									minDate={currentDate}
-									value={values.appointmentDate} 
-									showTimeSelect 
-									dateFormat="Pp"
-								/>
-								<ErrorMessage name="appointmentDate"/>
-							</div>
-							<Button type="submit" disabled={!isValid} variant="outlined">Submit</Button>
-						</Form>
-					)}
-				</Formik>
-			</CardContent>
-		</Card>
+		<div>
+			<Formik 
+				validationSchema={Schema}
+				onSubmit={onSubmit}
+				validateOnMount
+				initialValues={{
+					name: "",
+					email: "",
+					birthdate: "",
+					appointmentDate: ""}	
+				}
+			>
+				{({ values,  isValid, setFieldValue}) => (
+					<Form>
+						<div>
+							<label htmlFor="name">Name</label>
+							<Field id="name" name="name" type="text"/>
+							<ErrorMessage name="name"/>
+						</div>
+						<div>
+							<label htmlFor="email">Email Address</label>
+							<Field id="email" name="email" type="email"/>
+							<ErrorMessage name="email"/>
+						</div>
+						<div>
+							<label htmlFor="birthdate">Birthdate</label>
+							<DatePicker 
+								id="birthdate" 
+								name="birthdate" 
+								value={values.birthdate}
+								selected={birthdate}
+								onChange={(value) => {
+									setBirthdate(Date.parse(value));
+									setFieldValue("birthdate",value);
+									console.log(birthdate);
+								}}
+							/>
+							<ErrorMessage name="birthdate"/>
+						</div>
+						<div>
+							<label htmlFor="appointmentDate">Appointment Date</label>
+							<DatePicker id="appointmentDate" selected={appointmentDate} 
+								onChange={(value) => {
+									setAppointmentDate(Date.parse(value));
+									setFieldValue("appointmentDate", value);
+									console.log(appointmentDate);
+								}}
+								minDate={currentDate}
+								value={values.appointmentDate} 
+								showTimeSelect 
+								dateFormat="Pp"/>
+							<ErrorMessage name="appointmentDate"/>
+						</div>
+						<button type="submit" disabled={!isValid}>Submit</button>
+					</Form>
+				)}
+			</Formik>
+		</div>
 		
 	);
 };
 
-export default CardForm;
+export default CardComponent;
--- src/pages/Appointment/index.js
+++ src/pages/Appointment/index.js
@@ -1,14 +1,10 @@
-import { Container } from "@mui/material";
 import React from "react";
 //import { useFormik } from "formik";
 import Card from "../../components/Card";
 
 const Appointment = () => {
 	return (
-		<Container>
-			<Card />
-		</Container>
-		
+		<Card />
 	);
 };
 
